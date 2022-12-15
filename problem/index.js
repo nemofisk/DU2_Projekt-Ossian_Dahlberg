5f53a9c830e0c69b8eb98359fd4f1d72afad6a6a
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2022-12-15 12:13:26 +0100

    Updated code in index file for filter group toggling

--- problem/index.js
+++ problem/index.js
@@ -31,7 +31,8 @@ update_programmes();
 // Add Interaction of filter containers (select-deselect all filters in the container)
 // Example: Click anywhere on the language-filter-container and all the language filters
 // (spanska, svenska, engelska, franska) will toggle.
-
+const filter_containers = document.querySelectorAll(".filter_container");
+array_each(filter_containers, add_group_toggling);
 
 // VG
 // Add Interaction of button toggle-all-cities
